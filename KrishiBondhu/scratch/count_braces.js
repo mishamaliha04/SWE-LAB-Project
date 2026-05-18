d95bec15c3c7f4ccafbbe499bc79ef066f4640bf
jannatul-aurpy <jannatulaurpy@gmail.com>
2026-05-18 15:43:57 +0600

    Add files via upload

---
+++ KrishiBondhu/scratch/count_braces.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const content = fs.readFileSync('e:\\KrishiBondhu\\auth-frontend\\src\\pages\\FarmerDashboard.jsx', 'utf8');
+let open = 0;
+let close = 0;
+for (let i = 0; i < content.length; i++) {
+  if (content[i] === '{') open++;
+  if (content[i] === '}') close++;
+}
+console.log(`Open: ${open}, Close: ${close}`);
