27d7c7085946826a80b8a9b73429df42d7c16803
jannatul-aurpy <jannatulaurpy@gmail.com>
2026-05-18 15:49:12 +0600

    Delete KrishiBondhu/scratch directory

--- KrishiBondhu/scratch/count_braces.cjs
+++
@@ -1,9 +0,0 @@
-
-const fs = require('fs');
-const content = fs.readFileSync('e:/KrishiBondhu/auth-frontend/src/pages/FarmerDashboard.jsx', 'utf8');
-console.log(`Open: ${content.split('{').length - 1}`);
-console.log(`Close: ${content.split('}').length - 1}`);
-console.log(`Open Parentheses: ${content.split('(').length - 1}`);
-console.log(`Close Parentheses: ${content.split(')').length - 1}`);
-console.log(`Open Brackets: ${content.split('[').length - 1}`);
-console.log(`Close Brackets: ${content.split(']').length - 1}`);
